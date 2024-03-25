0b9c282395343edf08bca9b50a07f5d23dee0abb
economicdonut <mirfomar11@gmail.com>
2024-03-25 18:41:34 +0800

    boilerplate for book-charging-station

---
+++ book-charging-station/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_cors import CORS
+from routes.book_charger import book_charger_bp
+
+app = Flask(__name__)
+CORS(app)
+
+# Register Blueprints
+app.register_blueprint(book_charger_bp)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5004, debug=True)
