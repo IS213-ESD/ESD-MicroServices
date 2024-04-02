2a4aec894779ec9f9ea58aca187112250dd53b3c
Derrick-lkh <derrick872k@gmail.com>
2024-04-02 20:08:30 +0800

    COMPLEX-Notification - completed sending of notification for confirmation of booking

--- complex/handle-notifications/test.py
+++ complex/handle-notifications/test.py
@@ -10,7 +10,7 @@ def send_notification(queue_name, message):
     connection.close()
 
 
-message = {'booking_id': '123'}
+message = {'booking_id': 1, 'user_id': 'NVqPLXexIFUr3loYRl1GJgkfAep2'}
 json_message = json.dumps(message)
 print(json_message)
 send_notification('booking_confirmations', json_message)
